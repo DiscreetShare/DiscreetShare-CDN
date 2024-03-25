4605b728ed71181e4c23b6ca17aae29b2f3f7124
Nathan Laviolette <67888122+growtoups@users.noreply.github.com>
2024-03-24 22:37:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
+// netlify/functions/api.js
+
 const express = require('express');
 const crypto = require('crypto');
 const zlib = require('zlib');
-const fs = require('fs');
 const mime = require('mime-types');
-const path = require('path');
 const fetch = require('node-fetch'); // For making HTTP requests
 const mongoose = require('mongoose');
-const https = require('https');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+const router = express.Router();
+router.use(cors());
+
 mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-const app = express();
-const PORT = 8443;
-app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
-app.use(cors());
-
 const B2_BUCKET_URL = 'https://f005.backblazeb2.com/file/DiscreetShare-CDN/'; // Replace with your B2 bucket URL
-
-
 
 const BannedHash = mongoose.model('BannedHash', new mongoose.Schema({
     hash: String,
@@ -50,34 +46,26 @@ function decryptAndDecompress(data, encryptionKey, iv) {
     }
 }
 
-app.get('/:fileId', async (req, res) => {
+router.get('/:fileId', async (req, res) => {
     const fileId = req.params.fileId.trim();
 
-    // Validate ObjectId
     if (!mongoose.Types.ObjectId.isValid(fileId)) {
         return res.status(400).send('Invalid file ID format');
     }
 
     try {
-        // Look up the file in the database using the validated ObjectId
         const file = await File.findById(fileId);
 
         if (!file) {
             return res.status(404).send('File not found');
         }
 
-        // Construct the URL to the file in the Backblaze B2 bucket
         const fileUrl = B2_BUCKET_URL + file.encryptedFileName;
 
-        // Determine the MIME type from the original file extension
         const mimeType = mime.lookup(file.extension);
 
-        // Check if the MIME type is of an image or video
         if (mimeType && (mimeType.startsWith('image/') || mimeType.startsWith('video/'))) {
-            // Set the Content-Type for the response
             res.setHeader('Content-Type', mimeType);
-
-            // Fetch the file from the B2 bucket and stream it to the client
             const response = await fetch(fileUrl);
             if (!response.ok) {
                 console.error('Error fetching file from Backblaze B2:', response.statusText);
@@ -93,6 +81,4 @@ app.get('/:fileId', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = router;
